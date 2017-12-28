ec23fb09505f38f61e27bf528535a3b6c2a0459c
kusu <t.kusumoto0926@gmail.com>
2017-12-28 23:07:33 +0900

    Add state operation function

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,13 +3,30 @@ import React, { Component } from 'react';
 import Greeting from './greeting';
 
 class App extends Component {
+   // 親コンポーネントの初期の状態定義
+  constructor(props) {
+    super(props);
+    this.state = {
+      name : 'Bob',
+    };
+  }
+
+// setState　stateを更新する作業と、renderを更新の2つの役割
+  handleMouseOver() {
+    this.setState({ name: 'Bob' });
+  }
+
+  handleMouseOut() {
+    this.setState({ name: 'Mike' });
+  }
+// トップレベルのElementは一つでなければならない
   render() {
     return (
-      {// トップレベルのElementは一つでなければならない
-       }
-      <div>
-        <Greeting name='Bob'/>
-        <Greeting name='John'/>
+      <div
+        onMouseOver={() => this.handleMouseOver()}
+        onMouseOut={() => this.handleMouseOut()}
+      >
+        <Greeting name={this.state.name} />
       </div>
     );
   }
