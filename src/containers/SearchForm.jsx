2de60a0f82ce6dde10b401d016258cfd79c6a376
kusu <t.kusumoto0926@gmail.com>
2017-12-30 20:33:49 +0900

    Refactoring action

--- src/containers/SearchForm.jsx
+++ src/containers/SearchForm.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import { geocode } from '../domain/Geocoder';
-// import { searchHotelByLocation } from '../domain/HotelRepository';
+import { setPlace, startSearch } from '../actions/index.js';
 
 const SearchForm = props => (
   <form
     className="search-form"
     onSubmit={(e) => {
       e.preventDefault();
-      props.onSubmit(props.place);
+      props.startSearch(props.place);
     }}
   >
     <input
@@ -19,7 +18,7 @@ const SearchForm = props => (
       value={props.place}
       onChange={(e) => {
         e.preventDefault();
-        props.onPlaceChange(e.target.value);
+        props.setPlace(e.target.value);
       }}
     />
     <input className="submit-button" type="submit" value="検索" />
@@ -28,58 +27,13 @@ const SearchForm = props => (
 
 SearchForm.propTypes = {
   place: PropTypes.string.isRequired,
-  onPlaceChange: PropTypes.func.isRequired,
-  onSubmit: PropTypes.func.isRequired,
+  setPlace: PropTypes.func.isRequired,
+  startSearch: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = state => ({
-  place: state.place,
-});
-
-const mapDispatchToProps = dispatch => ({
-  onPlaceChange: place => dispatch({ type: 'CHANGE_PLACE', place }),
-  onSubmit: (place) => {
-    geocode(place)
-      .then(({ status, address, location }) => {
-        switch (status) {
-          case 'OK': {
-            dispatch({ type: 'GEOCODE_FETCHED', address, location });
-            // return searchHotelByLocation(location);
-            break;
-          }
-          case 'ZERO_RESULTS': {
-            // this.setErrorMessage('結果が見つかりませんでした');
-            break;
-          }
-          case 'OVER_QUERY_LIMIT': {
-            // this.setErrorMessage('クエリ数が割り当て量を超えています');
-            break;
-          }
-          case 'REQUEST_DENIED': {
-            // this.setErrorMessage('リクエストが拒否されています');
-            break;
-          }
-          case 'INVALID_REQUEST': {
-            // this.setErrorMessage('パラメータが間違っています');
-            break;
-          }
-          case 'UNKNOWN_ERROR': {
-            // this.setErrorMessage('サーバー エラーでリクエストが処理できませんでした');
-            break;
-          }
-          default: {
-            //this.setErrorMessage('エラーが発生しました');
-          }
-        }
-        return [];
-      });
-      //  .then((hotels) => {
-      //  this.setState({ hotels: sortedHotels(hotels, this.state.sortKey) });
-      //})
-      //.catch(() => {
-      //  this.setErrorMessage('通信に失敗しました');
-      //});
-  },
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(SearchForm);
+export default connect(
+  state => ({
+    place: state.place,
+  }),
+  { setPlace, startSearch },
+)(SearchForm);
